20b4be7fdc0f37ff3da575a496e4a7a459fca281
mise-en-dev <123107610+mise-en-dev@users.noreply.github.com>
2024-12-21 13:06:18 +0000

    usage 1.7.4

--- usage.rb
+++ usage.rb
@@ -1,8 +1,8 @@
 class Usage < Formula
   desc "Tool for CLIs"
   homepage "https://github.com/jdx/usage"
-  url "https://github.com/jdx/usage/archive/refs/tags/v1.3.1.tar.gz"
-  sha256 "80a88cd029a7b57d62486f64b49eaec5242100ed9831cbea82c6d6e392320317"
+  url "https://github.com/jdx/usage/archive/refs/tags/v1.7.4.tar.gz"
+  sha256 "a768c0aef980ebc83bf93f763372c23de6c3aad03a925f981209dbc24ad59072"
   license "MIT"
   head "https://github.com/jdx/usage.git", branch: "main"
 
